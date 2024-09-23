7bb931ea479bc1bc1d6ca7feb1186c13e9da15a9
Mirotin Artem <ai.mirotin@gmail.com>
2024-09-23 15:40:37 +0300

    Задание 2

---
+++ src/ru/stepup/task2/Employee.java
@@ -0,0 +1,34 @@
+package ru.stepup.task2;
+
+public class Employee {
+    private String name;
+    private int age;
+    private String position;
+
+    public Employee(String name, int age, String position) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
